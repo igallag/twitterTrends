dbd2489c6044796799fb8fd27af3019f6fccc591
Ian Gallgher <igallag93@gmail.com>
2020-07-10 13:01:18 -0400

    saving some routes I am going to change

---
+++ client/components/tempAccMock.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+const SupportServices = () => {
+  return (
+    <div>
+      <h1>This is the support services page</h1>
+      <div>
+        <h2>About</h2>
+        <p>This here is where your services are friend</p>
+        <ul>
+          <li>This is a service</li>
+          <li>This is a service also</li>
+          <li>This is the</li>
+        </ul>
+        <h2>Resources</h2>
+        <p>This here be links to the resources</p>
+      </div>
+    </div>
+  )
+}
+
+export default SupportServices
