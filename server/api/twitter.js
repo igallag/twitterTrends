48b14d7a2e4da549bfd1fea50cdfd26f56b6ce57
Ian Gallgher <igallag93@gmail.com>
2020-07-30 11:45:00 -0400

    added some console logs to the twitter api need to route it through express somehow

--- server/api/twitter.js
+++ server/api/twitter.js
@@ -1,13 +1,14 @@
-const router = require('express').Router()
-module.exports = router
+// const router = require('express').Router()
+// module.exports = router
 const Twit = require('twit')
+module.exports = Twit
 
 const T = new Twit({
   consumer_key: 'PGjwgSsQKrE5ytoR6Dwr7R3Z6',
   consumer_secret: '6U7ttBPv2RhCdHyMFsDPTIpBj9BDmoUWfT166XPxJLmA9ZlQPW',
   app_only_auth: true
 })
-
+console.log('is this auto calling?')
 T.get('followers/list', {screen_name: 'Gunsmokeisdead'}, function(
   err,
   data,
